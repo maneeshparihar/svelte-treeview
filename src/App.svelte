8e77dfef1eb95885aec3fe34576aa95af13beb70
wati <swatiagrawal501@gmail.com>
2020-06-01 11:41:13 +0530

    created new branch

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,32 @@
 <script>
-	export let name;
+	let value;
+	import JSONTree from './Root.svelte';
+
+	value = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "Swati",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<JSONTree {value} />
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/tree.svelte
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href='./tree.css'>
+<script src='./tree.js'>
+var tree = new Tree(document.getElementById('tree'));
+tree.json([{
+  name: 'file 1'
+}, {
+  name: 'file 2'
+}, {
+  name: 'folder 1',
+  open: true,
+  type: Tree.FOLDER,
+  selected: true,
+  children: [{
+    name: 'file 1'
+  }, {
+    name: 'file 2'
+  }, {
+    name: 'folder 1',
+    type: Tree.FOLDER
+  }]
+}]);
+ tree.on('fetch', folder => window.setTimeout(() => {
+  tree.file({
+    name: 'file 1'
+  }, folder);
+  tree.file({
+    name: 'file 2'
+  }, folder);
+  folder.resolve();
+}, 1000));
+</script>
+<div id="tree"></div>
+
