f291c820bf1d8bec44296aa8428f7877ead5eeb3
wati <swatiagrawal501@gmail.com>
2020-06-05 13:35:48 +0530

    initial commit with svelte editor

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,96 @@
 <script>
-	let value;
-	import JSONTree from './Root.svelte';
+	import JSONEditor from 'jsoneditor';
+	import {onMount} from 'svelte';
+	import '../node_modules/jsoneditor/dist/jsoneditor.min.css';
+	// import '../node_modules/jsoneditor/dist/img/jsoneditor-icons.svg'
+    // import {JSONEditor} from '../node_modules/jsoneditor/dist/jsoneditor.min.css';
+	let container, options, editor, updatedJson;
+	let modes= ['code', 'form', 'text', 'tree', 'view'];
+	export let json;
+	container = document.getElementById('jsoneditor');
 
-	value = {
-    "glossary": {
-        "title": "example glossary",
-		"GlossDiv": {
-            "title": "Swati",
-			"GlossList": {
-                "GlossEntry": {
-                    "ID": "SGML",
-					"SortAs": "SGML",
-					"GlossTerm": "Standard Generalized Markup Language",
-					"Acronym": "SGML",
-					"Abbrev": "ISO 8879:1986",
-					"GlossDef": {
-                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-						"GlossSeeAlso": ["GML", "XML"]
-                    },
-					"GlossSee": "markup"
-                }
-            }
-        }
-    }
-};
+	options = {
+		mode: 'tree',
+		modes: ['code', 'form', 'text', 'tree', 'view'], // allowed modes			
+		indentation: 4,
+		escapeUnicode: true,
+
+		onChange: function () {
+			updatedJson = editor.get();
+			console.log("UJ",updatedJson);
+			},
+
+		onModeChange: function (mode) {
+			let treeMode = document.getElementById('treeModeSelection');
+			let textMode = document.getElementById('textModeSelection');
+			treeMode.style.display = textMode.style.display = 'none';
+			if (mode === 'code' || mode === 'text') {
+				textMode.style.display = 'inline';
+			} else {
+				treeMode.style.display = 'inline';
+			}
+			},
+			
+		onTextSelectionChange: function(start, end, text) {
+			let rangeEl = document.getElementById('textRange');
+			rangeEl.innerHTML = 'start: ' + JSON.stringify(start) + ', end: ' + JSON.stringify(end);
+			let textEl = document.getElementById('selectedText');
+			textEl.innerHTML = text;
+			},
+
+		onSelectionChange: function(start, end) {      
+			let nodesEl = document.getElementById('selectedNodes');
+			nodesEl.innerHTML = '';
+			if (start) {
+				nodesEl.innerHTML = ('start: '  + JSON.stringify(start));
+				if (end) {
+				nodesEl.innerHTML += ('<br/>end: '  + JSON.stringify(end));
+				}
+				console.log(nodesEl.innerHTML);
+			}
+		}
+		};
+	
+	onMount( () => {	 
+
+		window.onunhandledrejection = (e) => {
+			alert(e.toString());
+		}
+
+		container = document.getElementById("jsoneditor")
+		editor = new JSONEditor(container, options)
+		editor.set(json);
+
+	});
 </script>
 
-<main>
-	<JSONTree {value} />
-</main>
+<style type="text/css">
+	body {
+		font: 10.5pt arial;
+		color: #4d4d4d;
+		line-height: 150%;
+		width: 500px;
+		padding-left: 40px;
+	}
+	#jsoneditor {
+		width: 500px;
+		height: 500px;
+}
+</style>
 
+<main>
+<div id="jsoneditor" style="width: 900px; height: 900px;"></div>
+	<body>
+		<form>
+			
+			<div id="textModeSelection" style="display:none;">
+				<b>Selection:</b><div id="textRange"></div>
+				<b>Text:</b><div id="selectedText"></div>
+			</div>
+			<div id="treeModeSelection">
+				<b>Selection:</b>
+				<div id="selectedNodes"></div>
+			</div>
+		</form>
+	</body>
+</main>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+	    json : {
+			"array": [1, 2, [3,4,5]],
+			"boolean": true,
+			"htmlcode": '&quot;',
+			"escaped_unicode": '\\u20b9',
+			"unicode": '\u20b9,\uD83D\uDCA9',
+			"return": '\n',
+			"null": null,
+			"number": 123,
+			"object": {"a": "b", "c": "d"},
+			"string": "Hello World",
+			"url": "http://jsoneditoronline.org"
+		}
 	}
 });
 
